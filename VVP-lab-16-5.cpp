98e41050b1f5ec0b244f5cd97484109f69ef4608
TheEvilPomeranian <90762280+TheEvilPomeranian@users.noreply.github.com>
2021-12-22 01:23:12 +0300

    Create VVP-lab-16-5.cpp

---
+++ VVP-lab-16-5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	cout << "Задача 5. \nДан массив A размера N. \nВывести вначале его элементы с нечетными номерами в порядке возрастания номеров, \nа затем — элементы с четными номерами в порядке убывания номеров" << endl;
+	int n, a, i;
+	cout << "Введите N: "; cin >> n;
+	int* arr = new int[n];
+	for ( i = 0; i < n; i++) {
+		cout << "arr[" << i << "] = "; cin >> arr[i];
+	}
+	cout << "Все числа с четным номером: " << endl;
+	for (i = 0; i < n; i++) {
+		if (i % 2 == 0) {
+			cout << "arr[" << i << "] = " << arr[i] << endl;
+
+		}
+	}
+	cout << "Все числа с нечетным номером: " << endl;
+	for (i = 0; i < n; i++) {
+		if (i % 2 != 0) {
+			cout << "arr[" << i << "] = " << arr[i] << endl;
+
+		}
+	}
+}
